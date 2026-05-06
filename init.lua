6d592cd88686cacb34c29e537be8fc33658d282e
sslime336 <sslime@yeah.net>
2026-05-06 16:17:35 +0800

    优化滚动效果(Ctrl+e,Ctrl+y)

--- init.lua
+++ init.lua
@@ -1,20 +1,24 @@
 local _ = require('basic')   -- 基础配置，如复用系统剪切板等
 local _ = require('keymaps') -- 键位映射
 
--- 安装插件
+-- 基础功能
 local _ = require("plugins.cmp")
 local _ = require("plugins.neotree")
 local _ = require("plugins.hop")
 local _ = require("plugins.lsp")
 local _ = require("plugins.which-key")
-local _ = require("plugins.lualine")
 local _ = require("plugins.gitsigns")
+
 local _ = require("plugins.telescope")
 local _ = require("plugins.comment")
 local _ = require("plugins.todo-comments")
-local _ = require("plugins.nvim-cursorline")
 local _ = require("plugins.nvim-autopairs")
-local _ = require("plugins.indent-blankline")
-local _ = require("plugins.tiny-inline-diagnostic")
 local _ = require("plugins.toggleterm")
+
+-- 体验优化类
+local _ = require("plugins.lualine")
+local _ = require("plugins.indent-blankline")
+local _ = require("plugins.nvim-cursorline")
+local _ = require("plugins.tiny-inline-diagnostic")
 local _ = require("plugins.vim-moonfly-colors")
+local _ = require("plugins.neoscroll")
---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,6 @@
+-- 丝滑滚动
+
+vim.pack.add({
+    'https://github.com/karb94/neoscroll.nvim'
+})
+require('neoscroll').setup({})
